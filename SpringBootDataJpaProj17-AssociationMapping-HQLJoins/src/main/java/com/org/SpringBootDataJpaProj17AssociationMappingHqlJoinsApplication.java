8a0ae56ddca0c6f5d29fee743909087418967cf5
Hemanth328 <hemanth.blueburn11@gmail.com>
2022-12-23 10:05:39 +0530

    Association Mapping HQL Joins

---
+++ SpringBootDataJpaProj17-AssociationMapping-HQLJoins/src/main/java/com/org/SpringBootDataJpaProj17AssociationMappingHqlJoinsApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaProj17AssociationMappingHqlJoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaProj17AssociationMappingHqlJoinsApplication.class, args);
+	}
+
+}
