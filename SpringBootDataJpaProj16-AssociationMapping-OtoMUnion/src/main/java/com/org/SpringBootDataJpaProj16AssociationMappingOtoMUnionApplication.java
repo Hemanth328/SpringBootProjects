37401a7cd6d9695405aab5985ca475e1cb047d03
dev328 <dev11@gmail.com>
2022-12-23 09:55:57 +0530

    Association Mapping Union

---
+++ SpringBootDataJpaProj16-AssociationMapping-OtoMUnion/src/main/java/com/org/SpringBootDataJpaProj16AssociationMappingOtoMUnionApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaProj16AssociationMappingOtoMUnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaProj16AssociationMappingOtoMUnionApplication.class, args);
+	}
+
+}
